54ccd0b9c89204a52abaf86d0f54b37c4b3ede70
Дмитрий <dimamagent59@gmail.com>
2026-03-16 20:21:38 +0300

    Start project

---
+++ FL_SharedLib/FL_SharedLib.cpp
@@ -0,0 +1,10 @@
+﻿// FL_SharedLib.cpp : Определяет функции для статической библиотеки.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: Это пример библиотечной функции.
+void fnFLSharedLib()
+{
+}
